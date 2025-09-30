7e9e993b7be614da3a43211a0487fae29ee4282b
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-09-30 17:19:45 +0100

    NEW: Created database connection

---
+++ src/dao/DatabaseConnection.java
@@ -0,0 +1,41 @@
+
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/bankV2";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "123";
+    private static DatabaseConnection instance;
+    private Connection connection;
+
+    private DatabaseConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            System.err.println("Driver PostgreSQL non trouvé : " + e.getMessage());
+            throw new SQLException("Impossible de charger le driver PostgreSQL", e);
+        }
+    }
+
+    public static DatabaseConnection getInstance() throws SQLException {
+        if (instance == null || instance.getConnection().isClosed()) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+}
